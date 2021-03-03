3496cc86e42d2da6a7b80197c9da5d0fcea9ab32
Jamie6978 <36363708+Jamie6978@users.noreply.github.com>
2021-03-03 15:48:06 +0100

    Init
    Init

---
+++ bot.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+require('buffer')
+require('nodejs-base64-converter')
+const { Client } = require('discord.js')
+const PREFIX = '$'
+
+
+
+const client = new Client({disableEveryone: true })
+
+client.on('ready', () => console.log("BEN KLAAR VOOR DE GEBRUIK WOLLAH"))
+
+client.on('message', async message => {
+    if(!message.content.startsWith(PREFIX)) return
+
+    const args = message.content.slice(PREFIX.length).trim().split(' ');
+
+
+    if (message.content.startsWith(`${PREFIX}kanker`)) {
+            message.channel.send(process.env.KANKER)
+        }
+        else if (message.content.startsWith(`${PREFIX}dood`)) {
+            message.channel.send('Ik ben dood.')
+        }
+})
+
+client.login(process.env.TOKEN)
+
+
+
+
