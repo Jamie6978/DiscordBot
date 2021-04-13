ee20f18d3c406f97f1aa9c9aef0f43ae72f79396
Jamie6978 <36363708+Jamie6978@users.noreply.github.com>
2021-04-13 13:41:46 +0200

    pog bot werkt

--- commands/pog.js
+++ commands/pog.js
@@ -1,35 +1,33 @@
-const levels = require('discord-xp');
 const canvacord = require('canvacord');
-const Discord = require('discord.js');
-const { description } = require('./kk');
-const { name } = require('./salade');
+const levels = require('discord-xp')
+const Discord = require('discord.js')
 
 
 module.exports = {
+    name: "pog",
 
-    name: "rank",
+    run: async (client, message, args) => {
 
-    execute: async (client, message, args) => {
         const target = message.author;
 
         const user = await levels.fetch(target.id, message.guild.id);
+        const neededXp = levels.xpFor(parseInt(user.level) + 1);
 
         if (!user) return message.reply('je hebt geen xp noob')
 
         const rank = new canvacord.Rank()
-            .setAvatar(message.author.displayAvatarURL({dynamic: false, format: 'png' }))
-            .setCurrentXP(user.xp)
-            .setLevel(user.level)
-         .setCurrentXP(user.xp)
-            .setRequiredXP(neededXp)
-            .setStatus(message.member.presence.status)
-         .setProgressBar('#1edb14', "COLOR")
-            .setUsername(message.author.username)
-            .setDiscriminator(message.author.discriminator)
+        .setAvatar(message.author.displayAvatarURL({dynamic: false, format: 'png' }))
+        .setCurrentXP(user.xp)
+        .setLevel(user.level)
+        .setCurrentXP(user.xp)
+        .setRequiredXP(neededXp)
+        .setStatus(message.member.presence.status)
+        .setProgressBar('#1edb14', "COLOR")
+        .setUsername(message.author.username)
+        .setDiscriminator(message.author.discriminator)
         rank.build()
-            .then(data => {
-            const attachment = new Discord.MessageAttachment(data, `kanker.png`)
-            message.channel.send(attachment);
+        .then(data => {
+        const attachment = new Discord.MessageAttachment(data, `kanker.png`)
+        message.channel.send(attachment);
         })
-    }
-}
+}}
