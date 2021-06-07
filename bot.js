d9630f21374b64c5d19693353ac24db1e01910b2
Jamie6978 <36363708+Jamie6978@users.noreply.github.com>
2021-06-07 21:02:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,9 @@ client.on('message', async message =>{
     else if (command == 'catnoir') {
         client.commands.get('catnoir').run(client, message, args);
     }
-
+    else if (command == 'rule34') {
+        client.commands.get('rule34').run(client, message, args);
+    }
 
     else if (command == 'lb' || command == 'leaderboard') {
         const rawLeaderboard = await levels.fetchLeaderboard(message.guild.id, 10)
